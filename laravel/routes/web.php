7fb54685f7669bf144c9851e78990ff19fbf56a6
zichenma <zma@liaisionedu.com>
2021-06-20 01:59:29 -0400

    3-2 laravel route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/home/hello', [HomeController::class, 'hello']);
+// 尽量不用 any
+Route::any('/home/hello', [HomeController::class, 'hello']);
+
+
+// 重定向：
+Route::get('here', function(){
+    return '重定向之前';
+});
+Route::get('there', function(){
+    return '重定向之后';
+});
+// 301 - 永久重定向  SEO tracking 之后地址
+Route::permanentRedirect('here', 'there');
+// 302 - 临时重定向  SEO tracking 之前地址
+Route::redirect('here', 'there');
